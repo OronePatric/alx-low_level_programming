cf349b47e86868d544472dae79201f55eba76515
Orone Patrick <patrickorone16@gmail.com>
2023-03-20 14:56:07 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints if number > 5 or number < 6 but not zero and zero
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int n, lastDigit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (lastDigit > 5 && n > 0)
+{
+printf("Last Digit of %d is %d and is greater than 5\n", n, lastDigit);
+}
+else if (lastDigit == 0)
+{
+printf("Last Digit of %d is %d  and is zero\n", n, lastDigit);
+}
+else if (lastDigit > 0 && lastDigit < 6)
+{
+printf("Last Digit of %d is %d and is less than 6 but not 0 ", n, lastDigit);
+}
+return (0);
+}
