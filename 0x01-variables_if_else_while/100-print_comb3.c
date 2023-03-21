bfc7573692d840c8de4ed54ab91e9b82f3666c1d
Orone Patrick <patrickorone16@gmail.com>
2023-03-20 20:02:49 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print combination of 2 digits.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int a = 0, b = 1;
+while ((a <= 8) || (b <= 9))
+{
+putchar(a + '0');
+putchar(b + '0');
+if (b != 9)
+{
+++b;
+}
+else
+{
+++a;
+b = a + 1;
+}
+if ((a <= 8) && (b <= 9))
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
