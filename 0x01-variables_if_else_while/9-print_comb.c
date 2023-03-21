6de8db5edd89afc5fccfaa286c0ec285898a3b4d
Orone Patrick <patrickorone16@gmail.com>
2023-03-20 18:04:01 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,12 +5,15 @@
  */
 int main(void)
 {
-int i = 0;
+int i;
+for (i = 0; i < 10; i++)
+{
 putchar(i + '0');
-for (i = 1; i < 10; i++)
+if (i < 9)
 {
 putchar(',');
-putchar(i + '0');
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
