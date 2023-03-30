74017d7261183f6f73914cff34e446c4fae1cc20
Orone Patrick <patrickorone16@gmail.com>
2023-03-29 23:36:49 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,14 @@
 #include "main.h"
 /**
- * main - 
+ * print_alphabet - print the alphabet in lower case:
+ *
+ */
+void print_alphabet(void){
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
+
